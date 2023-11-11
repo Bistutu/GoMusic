23446dc9605570e390bf56759e04e409a027353c
miankang.chen <91378285+Bistutu@users.noreply.github.com>
2023-11-11 20:37:04 +0800

    网易云歌单获取，优化cache缓存模型

--- logic/neteasy.go
+++ logic/neteasy.go
@@ -17,22 +17,24 @@ import (
 )
 
 const (
-	netEasyV1    = `.163.` // 正常链接
-	netEasyV2    = "163cn" // 短链接
-	netEasyRedis = "net_easy:%s"
-	netEasyUrlV6 = "https://music.163.com/api/v6/playlist/detail"
-	netEasyUrlV3 = "https://music.163.com/api/v3/song/detail"
+	netEasyV1       = `.163.` // 正常链接
+	netEasyV2       = "163cn" // 短链接
+	netEasyRedis    = "net:%v"
+	netEasyUrlV6    = "https://music.163.com/api/v6/playlist/detail"
+	netEasyUrlV3    = "https://music.163.com/api/v3/song/detail"
+	bracketsPattern = `\s\(.*\)|\s【.*】` // 去除（空格字符）括号及其中内容
 )
 
 var (
-	netEasyV1Regx = regexp.MustCompile(netEasyV1)
-	netEasyV2Regx = regexp.MustCompile(netEasyV2)
+	netEasyV1Regex = regexp.MustCompile(netEasyV1)
+	netEasyV2Regex = regexp.MustCompile(netEasyV2)
+	bracketsRegex  = regexp.MustCompile(bracketsPattern)
 )
 
 func NetEasyDiscover(link string) (string, error) {
 	var err error
 	// 如果是短链接，则转换为长链接
-	if netEasyV2Regx.MatchString(link) {
+	if netEasyV2Regex.MatchString(link) {
 		link, err = httputil.GetRedirectLocation(link)
 		if err != nil {
 			log.Errorf("fail to convert short to long: %v", err)
@@ -40,35 +42,22 @@ func NetEasyDiscover(link string) (string, error) {
 		}
 	}
 
+	// 获取歌单 id
 	id, err := utils.GetSongsId(link)
 	if err != nil {
 		log.Errorf("fail to parse url: %v", err)
 		return "", err
 	}
-	// 检查缓存
-	redisCache, err := cache.GetKey(fmt.Sprintf(netEasyRedis, id))
-	if err != nil {
-		log.Errorf("redis connect fail: %v", err)
-	}
-	// 1、如果缓存中存在的话
-	if redisCache != "" {
-		log.Infof("neteasy 命中缓存：%v", id)
-		return redisCache, nil
-	}
 
-	// 2、若缓存中不存在，取数据、缓存
+	// 第一次发请求，获取歌曲 id 列表
 	res, err := httputil.Post(netEasyUrlV6, strings.NewReader("id="+id))
 	if err != nil {
-		log.Errorf("fail to post: %v", err)
+		log.Errorf("fail to result: %v", err)
 		return "", err
 	}
 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Errorf("fail to read res body: %v", err)
-		return "", err
-	}
+	body, _ := io.ReadAll(res.Body)
 	netEasySongId := &models.NetEasySongId{}
 	err = json.Unmarshal(body, netEasySongId)
 	if err != nil {
@@ -77,36 +66,68 @@ func NetEasyDiscover(link string) (string, error) {
 	}
 	// 无权限访问
 	if netEasySongId.Code == 401 {
-		cache.SetKey(fmt.Sprintf(netEasyRedis, id), "") // redis 防击穿
 		log.Errorf("无权限访问, link: %v", link)
 		return "", errors.New("无权限访问")
 	}
-	trackIds := netEasySongId.Playlist.TrackIds         // 歌单歌曲 ID 列表
-	songsId := make([]*models.SongId, 0, len(trackIds)) // 歌曲 ID To []Uint
+	trackIds := netEasySongId.Playlist.TrackIds       // 歌单歌曲 ID 列表
+	songsIdString := make([]string, 0, len(trackIds)) // 歌曲 ID To []String
 	for _, v := range trackIds {
-		songsId = append(songsId, &models.SongId{Id: v.Id})
+		songsIdString = append(songsIdString, fmt.Sprintf(netEasyRedis, v.Id))
 	}
-	marshal, _ := json.Marshal(songsId)
 
-	post, err := httputil.Post(netEasyUrlV3, strings.NewReader("c="+string(marshal)))
+	// 尝试获取缓存
+	cacheResult, err := cache.MGet(songsIdString...)
 	if err != nil {
-		log.Errorf("fail to post: %v", err)
+		// 缓存获取失败，不退出
+		log.Errorf("fail to get key: %v", err)
+	}
+
+	missKey := make([]*models.SongId, 0)
+	resultList := make([]string, 0, len(trackIds))
+	for k, v := range cacheResult {
+		if v != nil {
+			resultList = append(resultList, v.(string))
+			continue
+		}
+		missKey = append(missKey, &models.SongId{Id: trackIds[k].Id})
+	}
+	// 全部命中，直接返回
+	if len(missKey) == 0 {
+		log.Infof("全部命中缓存（网易云）: %v", link)
+		songList := &models.SongList{
+			Name:  netEasySongId.Playlist.Name,
+			Songs: resultList,
+		}
+		bytes, _ := json.Marshal(songList)
+		return string(bytes), nil
+	}
+
+	// TODO 2023.11.11 考虑 missKey > 500 的情况，需要分批请求（errgroup）
+
+	// missKey 不为 0，第二次发请求，获取歌曲详情
+	marshal, _ := json.Marshal(missKey)
+	result, err := httputil.Post(netEasyUrlV3, strings.NewReader("c="+string(marshal)))
+	if err != nil {
+		log.Errorf("fail to result: %v", err)
 		return "", err
 	}
-	defer post.Body.Close()
+	defer result.Body.Close()
 
-	bytes, _ := io.ReadAll(post.Body)
+	bytes, _ := io.ReadAll(result.Body)
 	songs := &models.Songs{}
 	err = json.Unmarshal(bytes, &songs)
 	if err != nil {
 		log.Errorf("fail to unmarshal: %v", err)
 		return "", err
 	}
-	songsString := make([]string, 0, len(songs.Songs))
+
+	missKeyCacheMap := make(map[string]interface{})
+
 	builder := strings.Builder{}
 	for _, v := range songs.Songs {
 		builder.Reset()
-		builder.WriteString(v.Name)
+		// 去除多余符号
+		builder.WriteString(bracketsRegex.ReplaceAllString(v.Name, ""))
 		builder.WriteString(" - ")
 
 		authors := make([]string, 0, len(v.Ar))
@@ -115,22 +136,21 @@ func NetEasyDiscover(link string) (string, error) {
 		}
 		authorsString := strings.Join(authors, " / ")
 		builder.WriteString(authorsString)
-		songsString = append(songsString, builder.String())
+		song := builder.String()
+		missKeyCacheMap[fmt.Sprintf(netEasyRedis, v.Id)] = song
+		resultList = append(resultList, song)
 	}
-	songList := &models.SongList{
-		Name:  netEasySongId.Playlist.Name,
-		Songs: songsString,
-	}
-	bytes, err = json.Marshal(songList)
-	data := string(bytes)
+	// 写缓存
+	err = cache.MSet(missKeyCacheMap)
 	if err != nil {
-		log.Errorf("fail to marshal: %v", err)
-		return "", err
-	}
-	// 3、设置缓存
-	err = cache.SetKey(fmt.Sprintf(netEasyRedis, id), data)
-	if err != nil {
+		// 缓存写入失败，不退出
 		log.Errorf("fail to set key: %v", err)
 	}
-	return data, nil
+
+	songList := &models.SongList{
+		Name:  netEasySongId.Playlist.Name,
+		Songs: resultList,
+	}
+	bytes, err = json.Marshal(songList)
+	return string(bytes), nil
 }
--- repo/cache/redis.go
+++ repo/cache/redis.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
+
+	"GoMusic/initialize/log"
 )
 
 var (
@@ -31,4 +33,27 @@ func GetKey(key string) (string, error) {
 		return "", err
 	}
 	return val, nil
+}
+
+func MGet(keys ...string) ([]interface{}, error) {
+	result, err := rdb.MGet(ctx, keys...).Result()
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func MSet(kv map[string]interface{}) error {
+	pipeline := rdb.Pipeline()
+	for k, v := range kv {
+		// 缓存 24 小时
+		pipeline.Set(ctx, k, v, 24*time.Hour)
+	}
+	// 不关注单个命令的执行结果，只关注 pipeline 执行的结果
+	_, err := pipeline.Exec(ctx)
+	if err != nil {
+		log.Error("MSet error: ", err)
+		return err
+	}
+	return nil
 }
