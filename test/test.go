745fb39f4de7aeaf278b009c5abce2338beb6a96
miankang.chen <91378285+Bistutu@users.noreply.github.com>
2023-11-11 16:46:48 +0800

    网易云音乐json解析优化

--- test/test.go
+++ test/test.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+func main() {
+	msg := `{"Name":"zs","Age":18,"Msg":"haha"}`
+	p := &P{}
+	err := json.Unmarshal([]byte(msg), p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p)
+}
+
+type P struct {
+	Name string
+	Age  int
 }
