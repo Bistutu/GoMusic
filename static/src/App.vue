fa913103a1c032f01dc32913a5458b6e624d7485
ThinkStu <91378285+Bistutu@users.noreply.github.com>
2024-06-09 19:49:52 +0800

    add sponsor

--- static/src/App.vue
+++ static/src/App.vue
@@ -230,9 +230,11 @@ const sponsorData = [
   {'no': '6', 'name': '︷.噓.低調', 'sponsorship': '16'},
   {'no': '7', 'name': '汤晴', 'sponsorship': '6.6'},
   {'no': '8', 'name': 'autism゛', 'sponsorship': '3'},
-  {'no': '9', 'name': '王传坤', 'sponsorship': '1'},
-  {'no': '10', 'name': '木木子', 'sponsorship': '1'},
-  {'no': '11', 'name': '…', 'sponsorship': '…'}
+  {'no': '9', 'name': 'Jack', 'sponsorship': '1'},
+  {'no': '10', 'name': 'HK记者', 'sponsorship': '1'},
+  {'no': '11', 'name': '王传坤', 'sponsorship': '1'},
+  {'no': '12', 'name': '木木子', 'sponsorship': '1'},
+  {'no': '13', 'name': '…', 'sponsorship': '…'}
 ]
 
 function reset(msg) {
