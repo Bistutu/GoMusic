5dd8da7a4ba43099971136c008efb30c2fbbc1fd
ThinkStu <91378285+Bistutu@users.noreply.github.com>
2025-03-08 20:04:52 +0800

    更新Vue配置和页面标题，统一为"音乐迁移-GoMusic"

--- static/vue.config.js
+++ static/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: '音乐迁移-GoMusic'
+    }
+  }
 })
