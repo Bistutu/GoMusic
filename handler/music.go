ac66be765eb65d276da034138c211146026078c3
YoungLee <youngleepost@163.com>
2025-09-23 19:19:38 +0800

    添加倒序功能

--- handler/music.go
+++ handler/music.go
@@ -32,18 +32,19 @@ func MusicHandler(c *gin.Context) {
 	link := c.PostForm("url")
 	detailed := c.Query("detailed") == "true"
 	format := c.Query("format")
+	order := c.Query("order")
 	currentCount := counter.Add(1)
 
-	log.Infof("第 %v 次歌单请求：%v，详细歌曲名：%v，歌曲格式：%v", currentCount, link, detailed, format)
+	log.Infof("第 %v 次歌单请求：%v，详细歌曲名：%v，歌曲格式：%v，歌曲顺序：%v", currentCount, link, detailed, format, order)
 
 	// 路由到不同的音乐服务处理函数
 	switch {
 	case netEasyRegx.MatchString(link):
-		handleNetEasyMusic(c, link, detailed, format)
+		handleNetEasyMusic(c, link, detailed, format, order)
 	case qqMusicRegx.MatchString(link):
-		handleQQMusic(c, link, detailed, format)
+		handleQQMusic(c, link, detailed, format, order)
 	case qishuiMusicRegx.MatchString(link):
-		handleQiShuiMusic(c, link, detailed, format)
+		handleQiShuiMusic(c, link, detailed, format, order)
 	default:
 		log.Warnf("不支持的音乐链接格式: %s", link)
 		c.JSON(http.StatusBadRequest, &models.Result{Code: models.FailureCode, Msg: "不支持的音乐链接格式", Data: nil})
@@ -51,7 +52,7 @@ func MusicHandler(c *gin.Context) {
 }
 
 // handleNetEasyMusic 处理网易云音乐歌单
-func handleNetEasyMusic(c *gin.Context, link string, detailed bool, format string) {
+func handleNetEasyMusic(c *gin.Context, link string, detailed bool, format, order string) {
 	songList, err := logic.NetEasyDiscover(link, detailed)
 	if err != nil {
 		if strings.Contains(err.Error(), "无权限访问该歌单") {
@@ -65,12 +66,15 @@ func handleNetEasyMusic(c *gin.Context, link string, detailed bool, format strin
 
 	// 根据格式选项处理歌曲列表
 	formatSongList(songList, format)
+	
+	// 根据顺序选项处理歌曲列表
+	processSongOrder(songList, order)
 
 	c.JSON(http.StatusOK, &models.Result{Code: models.SuccessCode, Msg: SUCCESS, Data: songList})
 }
 
 // handleQQMusic 处理QQ音乐歌单
-func handleQQMusic(c *gin.Context, link string, detailed bool, format string) {
+func handleQQMusic(c *gin.Context, link string, detailed bool, format, order string) {
 	if link == "https://i.y.qq.com/v8/playsong.html" {
 		c.JSON(http.StatusBadRequest, &models.Result{Code: models.FailureCode, Msg: "无效歌单链接，请检查是否正确", Data: nil})
 		return
@@ -85,12 +89,15 @@ func handleQQMusic(c *gin.Context, link string, detailed bool, format string) {
 
 	// 根据格式选项处理歌曲列表
 	formatSongList(songList, format)
+	
+	// 根据顺序选项处理歌曲列表
+	processSongOrder(songList, order)
 
 	c.JSON(http.StatusOK, &models.Result{Code: models.SuccessCode, Msg: SUCCESS, Data: songList})
 }
 
 // handleQiShuiMusic 处理汽水音乐歌单
-func handleQiShuiMusic(c *gin.Context, link string, detailed bool, format string) {
+func handleQiShuiMusic(c *gin.Context, link string, detailed bool, format, order string) {
 	songList, err := logic.QiShuiMusicDiscover(link, detailed)
 	if err != nil {
 		log.Errorf("获取汽水音乐歌单失败: %v", err)
@@ -100,8 +107,26 @@ func handleQiShuiMusic(c *gin.Context, link string, detailed bool, format string
 
 	// 根据格式选项处理歌曲列表
 	formatSongList(songList, format)
+	
+	// 根据顺序选项处理歌曲列表
+	processSongOrder(songList, order)
 
 	c.JSON(http.StatusOK, &models.Result{Code: models.SuccessCode, Msg: SUCCESS, Data: songList})
+}
+
+// processSongOrder 根据指定的顺序处理歌曲列表
+func processSongOrder(songList *models.SongList, order string) {
+	if songList == nil || len(songList.Songs) == 0 {
+		return
+	}
+
+	// 如果是倒序，则反转歌曲列表
+	if order == "reverse" {
+		songs := songList.Songs
+		for i, j := 0, len(songs)-1; i < j; i, j = i+1, j-1 {
+			songs[i], songs[j] = songs[j], songs[i]
+		}
+	}
 }
 
 // formatSongList 根据指定的格式处理歌曲列表
